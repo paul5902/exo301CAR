cb25f43f817c2011589121c8dc8150d81eb5fd39
letailleurPaul <letailleur@b13p9.fil.univ-lille1.fr>
2018-05-17 12:09:33 +0200

    all tests ok

--- src/main/java/fr/eservices/drive/dao/CatalogDaoJPAImpl.java
+++ src/main/java/fr/eservices/drive/dao/CatalogDaoJPAImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TemporalType;
+import javax.xml.crypto.Data;
 
 import fr.eservices.drive.model.Article;
 import fr.eservices.drive.model.Category;
@@ -28,20 +29,31 @@ public class CatalogDaoJPAImpl implements CatalogDao {
 
 	@Override
 	public List<Category> getArticleCategories(int id) throws DataException {
+			Article article = em.find(Article.class,id);
+			if(article == null) {
+				throw new DataException("ko");
+			}
 			List<Category> categories = em.createQuery("select c from Article a join a.categories c where a.id=:idC",Category.class).setParameter("idC",id).getResultList();
 			return categories;
 
 	}
 
 	@Override
-	public List<Article> getCategoryContent(int categoryId) {
+	public List<Article> getCategoryContent(int categoryId) throws DataException {
+		Category category = em.find(Category.class,categoryId);
+		if(category == null) {
+			throw new DataException("ko");
+		}
 		List<Article> articles = em.createQuery("select a From Article a join a.categories c where c.id=:categoryId",Article.class).setParameter("categoryId",categoryId).getResultList();
 		return articles;
 	}
 
 	@Override
-	public List<Perishable> getPerished(Date day) {
+	public List<Perishable> getPerished(Date day) throws DataException {
 		List<Perishable> perishables = em.createQuery("select p from Perishable p where bestBefore<=:day").setParameter("day",day,TemporalType.DATE).getResultList();
+		if(perishables.size()>=200) {
+			throw new DataException("ko");
+		}
 		return perishables;
 	}
 
